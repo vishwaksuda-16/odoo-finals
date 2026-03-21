0efef23bc598457ac06949cb159d9ca37aa40a91
Sudharshan1305 <sudharshankrishnaalk@gmail.com>
2026-03-21 15:07:35 +0530

    Added App.jsx

--- frontend/fe/src/App.jsx
+++ frontend/fe/src/App.jsx
@@ -1,17 +1,54 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Sidebar from "./components/Sidebar";
+
 import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import ECOList from "./pages/ECOList";
+import ECOCreate from "./pages/ECOCreate";
+import ECODetail from "./pages/ECODetail";
+import ChangeView from "./pages/ChangeView";
+import Products from "./pages/Products";
+import ProductCreate from "./pages/ProductCreate";
+import BOM from "./pages/BOM";
+import BOMCreate from "./pages/BOMCreate";
+import Reporting from "./pages/Reporting";
+import Settings from "./pages/Settings";
+import ECOStages from "./pages/ECOStages";
 
 function App() {
   return (
-    <div className="flex">
+    <BrowserRouter>
+      <div className="flex">
 
-      {/* Sidebar */}
-      <Sidebar />
+        {/* Sidebar always visible */}
+        <Sidebar />
 
-      {/* Main Content */}
-      <Dashboard />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
 
-    </div>
+          <Route path="/ecos" element={<ECOList />} />
+          <Route path="/ecos/create" element={<ECOCreate />} />
+          <Route path="/ecos/detail" element={<ECODetail />} />
+          <Route path="/changes" element={<ChangeView />} />
+
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/create" element={<ProductCreate />} />
+
+          <Route path="/bom" element={<BOM />} />
+          <Route path="/bom/create" element={<BOMCreate />} />
+
+          <Route path="/reporting" element={<Reporting />} />
+
+          <Route path="/settings" element={<Settings />} />
+          <Route path="/settings/stages" element={<ECOStages />} />
+        </Routes>
+
+      </div>
+    </BrowserRouter>
   );
 }
 
